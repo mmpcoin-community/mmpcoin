870e98d74cfe7a436641be124f17124ba9d1341d
derek <derek@ourbit.com>
2024-12-13 11:41:48 +0800

    1

--- src/init.cpp
+++ src/init.cpp
@@ -512,8 +512,8 @@ std::string HelpMessage(HelpMessageMode mode)
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/bellscoin-community/bellscoinv2>";
-    const std::string URL_WEBSITE = "<https://github.com/bellscoin-community/bellscoinv2>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/bellscoin-community/mmpcoin>";
+    const std::string URL_WEBSITE = "<https://github.com/bellscoin-community/mmpcoin>";
 
     return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2013, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
